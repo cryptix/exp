57a2d3a492d4433c15d7e2d29bee9724796b15f8
Henry <cryptix@riseup.net>
2015-01-11 17:14:50 +0100

    webLogger shows usage of wslog

--- webLogger/main.go
+++ webLogger/main.go
@@ -1,40 +1,47 @@
 // webLogger shows an example usage of logging.GetHTTPHandler
-// it exposes log events through http
+// it exposes log events over websocket using my wslog package
 package main
 
 import (
 	"net/http"
 	"os"
-	"path/filepath"
 	"time"
 
-	"github.com/cryptix/go/logging"
+	"github.com/cryptix/go/logging/wslog"
+	"github.com/op/go-logging"
 )
 
-var l = logging.Logger("webLogger")
+var l = logging.MustGetLogger("example")
 
 func main() {
 	i := 0
 
-	fileLogOutput, err := os.Create(filepath.Base(os.Args[0]) + ".log")
-	logging.CheckFatal(err)
-	logging.SetupLogging(fileLogOutput)
+	stderrbackend := logging.NewLogBackend(os.Stderr, "stderr:", 0)
 
-	go func() {
-		time.Sleep(time.Second * 10)
-		fileLogOutput.Close()
-		os.Exit(0)
-	}()
+	wsbackend := wslog.NewBackend()
+	var format = logging.MustStringFormatter(
+		"%{longfile} ▶ %{id:03x} %{message}",
+	)
+
+	wsFormatter := logging.NewBackendFormatter(wsbackend, format)
+
+	logging.SetBackend(stderrbackend, wsFormatter)
 
 	go func() {
 		for {
-			l.Warningf("Logging event %d", i)
+			l.Warning("Logging event %d", i)
 			l.Debug("Hello")
 			i++
 			time.Sleep(time.Second)
 		}
 	}()
 
-	http.Handle("/log", logging.GetHTTPHandler())
-	logging.CheckFatal(http.ListenAndServe(":8080", nil))
+	http.Handle("/log", wsbackend)
+	check(http.ListenAndServe(":8080", nil))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }

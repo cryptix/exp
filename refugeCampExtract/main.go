f9a0fd94bf84d9470269223e896a28dd7f93f65a
Henry <cryptix@riseup.net>
2016-01-14 22:30:52 +0100

    rce: switch csv to mediawiki table templating

--- refugeCampExtract/main.go
+++ refugeCampExtract/main.go
@@ -1,7 +1,18 @@
+/*
+table generator for
+
+	https://wiki.freifunk.net/index.php?title=Hamburg/Fl%C3%BCchtlinge#Liste_von_Unterk.C3.BCnften
+
+TODO
+
+1. consolidate existing data (status, doku links, contact person)
+
+2. intermediate datastorage to detect upstream changes
+
+*/
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +20,7 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"text/template"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -16,28 +28,51 @@ import (
 
 // from: https://www.hamburg.de/fluechtlinge-unterbringung-standorte/
 var urls = []string{
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4372596/unterbringung-altona/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373118/unterbringung-bergedorf/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373120/unterbringung-eimsbuettel/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373122/unterbringung-harburg/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373128/unterbringung-mitte/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373126/unterbringung-nord/",
-	"http://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373124/unterbringung-wandsbek/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4372596/unterbringung-altona/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373118/unterbringung-bergedorf/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373120/unterbringung-eimsbuettel/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373122/unterbringung-harburg/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373128/unterbringung-mitte/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373126/unterbringung-nord/",
+	"https://www.hamburg.de/fluechtlinge-unterbringung-standorte/4373124/unterbringung-wandsbek/",
 }
 
 var re = regexp.MustCompile(`\(Stand ([0-9]+\.[0-9]+\.[0-9]+)\)`)
 
 type result struct {
-	bezirk, stadtteil string
-	strasse, plaetze  string
-	wohnart           string
+	Bezirk, Stadtteil string
+	Strasse, Plaetze  string
+	Wohnart           string
 
-	date time.Time
+	Date time.Time
 
 	err error
 }
 
-var csvFname = flag.String("fname", "list.csv", "filename to write to")
+var csvFname = flag.String("fname", "list.tbl", "filename to write to")
+
+var mediaWikiTpl = template.Must(template.New("mediaTable").Parse(`
+{| class="mw-datatable sortable toptextcells"
+! '''Bezirk'''
+! '''Stadtteil'''
+! '''Straße'''
+! '''Plätze'''
+! '''Wohnart'''
+! '''Anprechpartner*'''
+! '''Doku'''
+! '''Status**'''
+{{range .}}
+|-
+| {{.Bezirk}}
+| {{.Stadtteil}}
+| {{.Strasse}}
+| {{.Plaetze}}
+| {{.Wohnart}}
+| TODOKontakt
+| TODODoku
+| TODOStatus{{end}}
+|}
+`))
 
 func main() {
 	flag.Parse()
@@ -45,28 +80,13 @@ func main() {
 	w, err := os.Create(*csvFname)
 	check(err)
 
-	csvW := csv.NewWriter(w)
-
-	// csv header row
-	err = csvW.Write([]string{"Bezirk", "Stadtteil", "Strasse", "Plaetze", "Wohnart"})
-	check(err)
-
+	var rs []result
 	for r := range fetchAndParse(urls) {
 		check(r.err)
-
-		err = csvW.Write([]string{
-			r.bezirk,
-			r.stadtteil,
-			r.strasse,
-			r.plaetze,
-			r.wohnart,
-		})
-		check(err)
+		rs = append(rs, r)
 	}
 
-	csvW.Flush()
-
-	err = csvW.Error()
+	err = mediaWikiTpl.Execute(w, rs)
 	check(err)
 
 	err = w.Close()
@@ -95,7 +115,7 @@ func fetchAndParse(urls []string) <-chan result {
 				// strip out the Bezirk from the url
 				var p = "unterbringung-"
 				i := strings.LastIndex(u, p)
-				r.bezirk = strings.Title(u[i+len(p) : len(u)-1])
+				r.Bezirk = strings.Title(u[i+len(p) : len(u)-1])
 
 				introText := doc.Find("p.intro").Text()
 				m := re.FindStringSubmatch(introText)
@@ -104,7 +124,7 @@ func fetchAndParse(urls []string) <-chan result {
 					c <- r
 					return
 				}
-				r.date, err = time.Parse("02.01.2006", m[1])
+				r.Date, err = time.Parse("02.01.2006", m[1])
 				if err != nil {
 					r.err = err
 					c <- r
@@ -113,7 +133,7 @@ func fetchAndParse(urls []string) <-chan result {
 
 				tables := doc.Find("div.richtext table")
 				if tables.Length() != 2 {
-					r.err = fmt.Errorf("rce: expected 2 tables on stadtteil-page")
+					r.err = fmt.Errorf("rce: expected 2 tables on Stadtteil-page")
 					c <- r
 					return
 				}
@@ -127,7 +147,7 @@ func fetchAndParse(urls []string) <-chan result {
 
 				rows := t.Find("tr")
 
-				log.Printf("%s: Bestehende Standorte: %d (Stand: %s)", r.bezirk, rows.Length()-1, r.date.Format("02.01.2006"))
+				log.Printf("%s: Bestehende Standorte: %d (Stand: %s)", r.Bezirk, rows.Length()-1, r.Date.Format("02.01.2006"))
 
 				rows.Each(func(i int, row *goquery.Selection) {
 					if i == 0 {
@@ -135,11 +155,10 @@ func fetchAndParse(urls []string) <-chan result {
 						return
 					}
 					fields := row.Find("td")
-
-					r.stadtteil = fields.Eq(0).Text()
-					r.strasse = fields.Eq(1).Text()
-					r.plaetze = fields.Eq(2).Text()
-					r.wohnart = fields.Eq(3).Text()
+					r.Stadtteil = fields.Eq(0).Text()
+					r.Strasse = fields.Eq(1).Text()
+					r.Plaetze = fields.Eq(2).Text()
+					r.Wohnart = fields.Eq(3).Text()
 					c <- r
 				})
 			}(u)
